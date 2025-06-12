73c9c34126f04b51ffb1ab8f77b464b5ff9697da
Quan Truong <kubim298@gmail.com>
2025-06-12 00:13:47 -0700

    Fix Ci error on Github

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
-use App\Livewire\Bookmark\Index;
+use App\Livewire\BookMark\Index;
 
 Route::get('/', function () {
     return view('welcome');
