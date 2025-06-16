6c0a09f8327714d76e045d2ee7412219bb3b1d8c
Quan Truong <kubim298@gmail.com>
2025-06-15 22:46:20 -0700

    Resort views files

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,20 @@ Route::get('/register', function () {
 
 
 
-Route::view('dashboard', 'dashboard')
+Route::view('dashboard', 'layouts.dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+// Route::middleware(['auth'])->group(function () {
+//     Route::redirect('settings', 'settings/profile');
 
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
+//     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+//     Volt::route('settings/password', 'settings.password')->name('settings.password');
+//     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+// });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/client-dashboard', Index::class)->name('client.dashboard');
-});
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('/client-dashboard', Index::class)->name('client.dashboard');
+// });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
