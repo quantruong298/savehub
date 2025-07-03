cf933375c66f4684f5673411ad4c4d7e93880cc6
Quan Truong <kubim298@gmail.com>
2025-07-03 01:09:59 -0700

    Show/Hide Password

--- resources/views/livewire/auth/login-form.blade.php
+++ resources/views/livewire/auth/login-form.blade.php
@@ -22,8 +22,33 @@
             Password
             <span class="text-red-600">*</span>
         </label>
-        <input type="password" placeholder="Enter your password" wire:model="password" required
-            class="w-full px-4 py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-600 focus:border-transparent outline-none transition-all" />
+        <div class="relative" x-data="{ show: false }">
+            <input :type="show ? 'text' : 'password'" placeholder="Enter your password" wire:model="password" required 
+                class="w-full px-4 py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-600 focus:border-transparent outline-none transition-all" />
+            <button type="button"
+                class="absolute inset-y-0 right-0 flex items-center px-3 text-gray-400 hover:text-gray-600 transition-colors focus:outline-none cursor-pointer"
+                tabindex="-1"
+                @click="show = !show"
+                :aria-label="show ? 'Hide password' : 'Show password'">
+                <template x-if="!show">
+                    <!-- Eye Icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                    </svg>
+                </template>
+                <template x-if="show">
+                    <!-- Eye Slash Icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.956 9.956 0 012.174-3.362m2.735-2.322A9.956 9.956 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.965 9.965 0 01-4.103 5.527M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" />
+                    </svg>
+                </template>
+            </button>
+        </div>    
     </div>
 
     <div class="flex items-center space-x-2" >
