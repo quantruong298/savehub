e24eff09d3a949c9425e0b0778b3f9b6c7efd8cd
Quan Truong <kubim298@gmail.com>
2025-07-07 20:37:15 -0700

    basic UI

--- resources/views/dashboard/folders.blade.php
+++ resources/views/dashboard/folders.blade.php
@@ -57,7 +57,7 @@
         </div>
     </div>
     {{-- Read Components --}}
-    <livewire:bookmark.read/>
+    <livewire:folder.read/>
 
     {{-- Update Components --}}
     <livewire:bookmark.update/>
