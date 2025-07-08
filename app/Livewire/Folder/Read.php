026fc1cf932665561a861712fcc2aa1f51683be3
Quan Truong <kubim298@gmail.com>
2025-07-07 22:29:16 -0700

    Done with Folder-read

--- app/Livewire/Folder/Read.php
+++ app/Livewire/Folder/Read.php
@@ -3,11 +3,23 @@
 namespace App\Livewire\Folder;
 
 use Livewire\Component;
+use App\Models\Folder;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
+use Livewire\WithPagination;
 
 class Read extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.folder.read');
+        $folders = Folder::where('user_id', auth()->id())
+            ->withCount('bookmarks')
+            ->latest()
+            ->paginate(8);
+        return view('livewire.folder.read', [
+            'folders' => $folders,
+        ]);
     }
 }
